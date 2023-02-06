5adda954fb59d11d37fc53f586e6d3e8236f948c
Mohamed Hamisa <108889205+MohamedHamisa@users.noreply.github.com>
2023-02-06 23:04:16 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,9 @@
+class Solution:
+    def middleNode(self, head):
+        tmp = head
+        while tmp and tmp.next:
+            head = head.next
+            tmp = tmp.next.next
+        return head
+      
+,               
